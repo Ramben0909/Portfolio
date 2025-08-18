fc72c2995fddcfb7badda4cca51213ac5be9a55a
Ritam Bhattacharya <ritam07mc@gmail.com>
2025-08-18 19:26:15 +0530

    modified Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -13,18 +11,36 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a
+            href="mailto:ritam09bhattacharya@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ritam09bhattacharya@gmail.com
+          </a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a
+            href="https://www.linkedin.com/in/ritam-bhattacharya-463248263/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            linkedin.com/in/ritam-bhattacharya-463248263
+          </a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a
+            href="https://github.com/Ramben0909"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.com/Ramben0909
+          </a>
         </li>
       </ul>
     </footer>
